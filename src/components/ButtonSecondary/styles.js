4955859696ee5df62cb3066f7f336573d3ef37a5
Macsilva40 <mdprints@hotmail.com>
2021-06-26 15:35:48 -0400

    criacao do componete texto e botao

---
+++ src/components/ButtonSecondary/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const styles = makeStyles(() => ({
+  button: {
+    backgroundColor: "white",
+    color: "#3f31b5",
+    borderColor: "#3f31b5",
+    borderWidth: 1,
+    borderStyle: "solid",
+  },
+}));
+
+export default styles;
